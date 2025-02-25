a65aa8ac4e102ef77a48b24c46c07e93a6e462e5
AlinePBrandao <alinep02brandao@gmail.com>
2025-02-25 12:39:38 -0300

    Módulo 6- Introdução a Métodos, Instanciação e Classe

--- Exercicios_Aula/src/M6_OOJava/Area/entities/Triangle.java
+++ Exercicios_Aula/src/M6_OOJava/Area/entities/Triangle.java
@@ -4,4 +4,13 @@ public class Triangle {
     public double a;
     public double b;
     public double c;
+
+    public double area(){ //Não recebe parametros pois os dados necessários estão contidos na classe
+        double p = (a + b + c) / 2.0; //atributos sa classe, aqui não temos objetos
+        double result = Math.sqrt(p * (p-a) * (p-b) * (p-c));
+        return result;
+
+        /*ou  simplesmente
+        return Math.sqrt(p * (p-a) * (p-b) * (p-c)); */
+    }
 }
