28387aa55b6420419941f08f2f225168043b5440
AlinePBrandao <alinep02brandao@gmail.com>
2025-04-15 13:36:57 -0300

    Módulo 8 - Vetores P.2

---
+++ Exercicios_Aula/src/M8_ArrayAndLists/application/Program.java
@@ -0,0 +1,35 @@
+package M8_ArrayAndLists.application;
+
+import M8_ArrayAndLists.entities.Products;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Number of products: ");
+        int n = sc.nextInt(); //tamanho do vetor
+        Products[] vect = new Products[n]; //instanciou classe Products
+
+        System.out.println("Enter the Name and Price of the products: ");
+        //corpo do vetor + instância
+        for (int i=0; i<n; i++){ //vetor
+            sc.nextLine(); //quebra de linha pendente
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Products(name, price); //instância do novo produto, vec[i] aponta para ele
+        }
+
+        double sum = 0;
+        for (int i=0; i<n; i++){
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / n;
+        System.out.printf("AVAREGE PRICE = %.2f %n", avg);
+
+        sc.close();
+    }
+}
