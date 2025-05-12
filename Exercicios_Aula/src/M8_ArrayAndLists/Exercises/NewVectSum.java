94f9ff293d957b5b3d1627cecf1a917667f45787
AlinePBrandao <alinep02brandao@gmail.com>
2025-05-12 20:13:11 -0300

    Módulo 8 - Vetores - Ex.6

---
+++ Exercicios_Aula/src/M8_ArrayAndLists/Exercises/NewVectSum.java
@@ -0,0 +1,37 @@
+package M8_ArrayAndLists.Exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NewVectSum {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many number in the vector are you going to enter? ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
+
+        System.out.println("Enter the values of vector A: ");
+        for (int i = 0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the values of vector B: ");
+        for (int i = 0; i<n; i++){
+            b[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i<n; i++){
+            c[i] = a[i] + b[i];
+        }
+
+        System.out.println("Resultant vector: ");
+        for (int i = 0; i<n; i++){
+            System.out.printf("%d\n", c[i]);
+        }
+        sc.close();
+    }
+}
