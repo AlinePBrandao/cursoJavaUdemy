d4d6a06028ce85a8089c5b83c851d3ea9cd0491c
AlinePBrandao <alinep02brandao@gmail.com>
2025-06-27 23:42:12 -0300

    Módulo 9 - Matrizes - Exercicio resolvido - add propriedade length

--- Exercicios_Aula/src/M9_Lists/Matriz/Matrizes.java
+++ Exercicios_Aula/src/M9_Lists/Matriz/Matrizes.java
@@ -9,28 +9,28 @@ public class Matrizes {
         int n = sc.nextInt();
         int[][] mat = new int[n][n]; //arranjo bidiecional - arranjo dado pela quantidade de colchetes
 
-        for(int i=0; i<n; i++){ //for para percorrer as linhas
-            for (int j = 0; j <n; j++){ //for para percorrer as colunas
+        for(int i=0; i< mat.length; i++){ //for para percorrer as linhas
+            for (int j = 0; j <mat[i].length; j++){ //for para percorrer as colunas
                 mat[i][j] = sc.nextInt(); //inserir valor correspondente a cada espaço
             }
         }
 
         System.out.println("Main diagonal: ");
-        for(int i=0; i<n; i++){
+        for(int i=0; i<mat.length; i++){
             System.out.print(mat[i][i] + " "); //elemento na mesma posição (linha 0 coluna 0, 1 e 1, 2 e 2)
         }
         System.out.println();
 
         int count = 0;
-        for (int i=0; i<n; i++){
-            for (int j = 0; j <n; j++){
+        for (int i=0; i<mat.length; i++){
+            for (int j = 0; j <mat[i].length; j++){
                 if (mat[i][j] < 0){
                     count++;
                 }
             }
         }
 
-        System.out.println("Negative numbers: " + count);
+        System.out.println("Negative numbers= " + count);
         sc.close();
     }
 }
