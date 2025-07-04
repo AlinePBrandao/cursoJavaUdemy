a8eee745809c5cb1a07a30622d707a20bbbbf325
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-04 19:57:24 -0300

    Módulo 10 - Data e Tempo- formatação DateTime fuso horário

--- Exercicios_Aula/src/M10_DateTime/FormaterDateTime.java
+++ Exercicios_Aula/src/M10_DateTime/FormaterDateTime.java
@@ -3,6 +3,7 @@ package M10_DateTime;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class FormaterDateTime {
@@ -11,12 +12,15 @@ public class FormaterDateTime {
         //Converter data-hora p/ texto
         LocalDate d01 = LocalDate.parse("2025-07-04");
         LocalDateTime d02 = LocalDateTime.parse("2025-07-04T01:30:26");
+        Instant d03 = Instant.parse("2025-07-04T01:30:26Z"); //data ambigua dependendo do fuso
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); //estabelece zona + fuso do computador
 
         System.out.println("d01 = " + d01.format(fmt1)); //respeita formato estabelecido
         System.out.println("d02 = " + d02.format(fmt2)); //respeita formato com hora
+        System.out.println("d03 = " + fmt3.format(d03)); //considerando horario sistema local a partir do fuso estabelecido (nesse caso -3:00)
 
 
 
