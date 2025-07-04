5d88570a48a9141f4f1bbd1c141b0068e714d58c
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-04 19:49:31 -0300

    Módulo 10 - Data e Tempo- formatação DateTime com hora

--- Exercicios_Aula/src/M10_DateTime/FormaterDateTime.java
+++ Exercicios_Aula/src/M10_DateTime/FormaterDateTime.java
@@ -10,11 +10,17 @@ public class FormaterDateTime {
 
         //Converter data-hora p/ texto
         LocalDate d01 = LocalDate.parse("2025-07-04");
+        LocalDateTime d02 = LocalDateTime.parse("2025-07-04T01:30:26");
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("d01 = " + d01.format(fmt1)); //respeita formato estabelecido
-        System.out.println("d01 = " + fmt1.format(d01)); //imprimindo a partir dele
-        System.out.println("d01 = " + d01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))); //Instanciação direta, sem reaproveitamento
+        System.out.println("d02 = " + d02.format(fmt2)); //respeita formato com hora
+
+
+
+        //System.out.println("d01 = " + fmt1.format(d01)); //imprimindo a partir dele
+        //System.out.println("d01 = " + d01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))); //Instanciação direta, sem reaproveitamento
     }
 }
