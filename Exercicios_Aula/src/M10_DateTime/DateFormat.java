000188217b0d438c3e675e40e2f10843535c592c
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-08 17:35:10 -0300

    Módulo 10 - Date - now

--- Exercicios_Aula/src/M10_DateTime/DateFormat.java
+++ Exercicios_Aula/src/M10_DateTime/DateFormat.java
@@ -10,10 +10,18 @@ public class DateFormat {
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-        Date y1 = sdf1.parse("08/07/2025"); //converte a data respeotando a formatação
+        Date x1 = new Date(); //data com instante atual
+
+        Date y1 = sdf1.parse("08/07/2025"); //converte a data respeitando a formatação
         Date y2 = sdf2.parse("08/07/2025 17:27:45");
 
-        System.out.println(y1);
-        System.out.println(y2);
+        //formato padrão Tue Jul 08 17:27:45 BRT 2025
+        System.out.println("y1 basic form: " + y1);
+        System.out.println("y2 basic form: " + y2);
+
+        //Formato com hora, min e seg
+        System.out.println("x1: " +sdf2.format(x1));
+        System.out.println("y1 formatter: " +sdf2.format(y1));
+        System.out.println("y2 formatter: " +sdf2.format(y2));
     }
 }
