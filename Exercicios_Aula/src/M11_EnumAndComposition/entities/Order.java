df6551cf58afa51995af881728329243d0fe4c1a
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-09 23:24:02 -0300

    Módulo 11- Enum and Compositor - Exemplo Ciclo de Vida de um pedido (Conversão String-enum)

--- Exercicios_Aula/src/M11_EnumAndComposition/entities/Order.java
+++ Exercicios_Aula/src/M11_EnumAndComposition/entities/Order.java
@@ -46,10 +46,10 @@ public class Order {
 
     @Override
     public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", moment=" + moment +
-                ", status=" + status +
-                '}';
+        return "Order{ " +
+                "id= " + id +
+                ", moment= " + moment +
+                ", status= " + status +
+                " }";
     }
 }
