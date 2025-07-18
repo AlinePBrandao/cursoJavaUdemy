f74396b7ed8d46c1ce114fa475712fecd7f02765
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-18 15:49:17 -0300

    Módulo 11- Composition - Exercício resolvido 2 - Program- instancia Comment and Post

--- Exercicios_Aula/src/M11_EnumAndComposition/StringBuilder/application/Program.java
+++ Exercicios_Aula/src/M11_EnumAndComposition/StringBuilder/application/Program.java
@@ -1,4 +1,22 @@
 package M11_EnumAndComposition.StringBuilder.application;
 
+import M11_EnumAndComposition.StringBuilder.entities.Comment;
+import M11_EnumAndComposition.StringBuilder.entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's awesome!");
+        Post p1 = new Post(
+                sdf.parse("21/06/2025 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+    }
 }
