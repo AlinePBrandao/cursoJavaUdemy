cb7aed6059e2a4fed7dfe0a0540d1eda23a22064
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-18 15:17:32 -0300

    Módulo 11- Composition - Exercício resolvido 2 - Post - constructor e get set

--- Exercicios_Aula/src/M11_EnumAndComposition/StringBuilder/entities/Post.java
+++ Exercicios_Aula/src/M11_EnumAndComposition/StringBuilder/entities/Post.java
@@ -11,5 +11,52 @@ public class Post {
     private String content;
     private Integer likes;
 
-    private List<Comment> comments = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>(); //associação
+
+    public Post(Date moment, String title, String content, Integer likes){
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
