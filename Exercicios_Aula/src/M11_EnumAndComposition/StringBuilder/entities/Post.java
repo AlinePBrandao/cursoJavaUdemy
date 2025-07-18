351fb308de33b4d0df6e41ee698af087fe1d5c9e
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-18 15:13:24 -0300

    Módulo 11- Composition - Exercício resolvido 2 - Post - declaração atributos

--- Exercicios_Aula/src/M11_EnumAndComposition/StringBuilder/entities/Post.java
+++ Exercicios_Aula/src/M11_EnumAndComposition/StringBuilder/entities/Post.java
@@ -1,3 +1,15 @@
 package M11_EnumAndComposition.StringBuilder.entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Post {
+
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer likes;
+
+
+}
