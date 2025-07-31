8db7de0cbcc36e3ab2037eb1290c5d902d256795
AlinePBrandao <alinep02brandao@gmail.com>
2025-07-31 17:44:32 -0300

    :construction: feat: creating Order class- composição de objetos

--- Exercicios_Aula/src/M11_EnumAndComposition/Order/entities/Order.java
+++ Exercicios_Aula/src/M11_EnumAndComposition/Order/entities/Order.java
@@ -1,9 +1,17 @@
 package M11_EnumAndComposition.Order.entities;
 
+import M11_EnumAndComposition.Order.entities.enums.OrderStatus;
+
 import java.util.Date;
+import java.util.List;
 
 public class Order {
 
     private Date moment;
+    private OrderStatus orderStatus;
+
+    //Associações - Composição de Objetos
+    private Client client; //Order possui 1 Client
+    private List<OrderItem> items; //Order possui vários Items
 
 }
