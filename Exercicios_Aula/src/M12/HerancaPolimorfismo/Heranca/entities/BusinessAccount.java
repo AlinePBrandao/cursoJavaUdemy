4bec7fcc0f982518d301bb9edf29f7c9a8ee4863
AlinePBrandao <alinep02brandao@gmail.com>
2025-08-06 13:09:19 -0300

    :construction: feat: BusinessAccount - herança e constructor com superclass

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/entities/BusinessAccount.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/entities/BusinessAccount.java
@@ -1,4 +1,13 @@
 package M12.HerancaPolimorfismo.Heranca.entities;
 
-public class BusinessAccount {
+public class BusinessAccount extends Account{
+
+    private Double loanLimit;
+
+    //constructor terá os argumentos da classe Account
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance); //superclasse para não repetir as atribuições
+        this.loanLimit = loanLimit;
+    }
 }
