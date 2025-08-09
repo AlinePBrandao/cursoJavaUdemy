16a9f12a585e66508b2e6b05879ed461ee14243f
AlinePBrandao <alinep02brandao@gmail.com>
2025-08-08 21:27:37 -0300

    :construction: feat: BusinessAccount - método loan e novos comentários

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/application/Program.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/application/Program.java
@@ -1,4 +1,12 @@
 package M12.HerancaPolimorfismo.Heranca.application;
 
+import M12.HerancaPolimorfismo.Heranca.entities.BusinessAccount;
+
 public class Program {
+    public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.withdraw(); //Possibilita o acesso a todos os atrivutos da lasse
+    }
 }
--- Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/entities/BusinessAccount.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/entities/BusinessAccount.java
@@ -1,13 +1,27 @@
 package M12.HerancaPolimorfismo.Heranca.entities;
 
 public class BusinessAccount extends Account{
-
+//extends "importa" a classe
     private Double loanLimit;
 
     //constructor terá os argumentos da classe Account
-
     public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
         super(number, holder, balance); //superclasse para não repetir as atribuições
+        //super executa a lógica da classe base - Account
         this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if (amount <= loanLimit){ //dinheiro só pode sem emprestado se a quantia for menor que o limite de empréstimo
+            deposit(amount); //dinheiro entra na conta por meio de um depósito
+        }
     }
 }
