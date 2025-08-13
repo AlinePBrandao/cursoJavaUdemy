f818316a7f36f7fa65cc66703c30b86c90e9e01b
AlinePBrandao <alinep02brandao@gmail.com>
2025-08-13 16:32:48 -0300

    :construction: build: Program - variáveis e atribuições de valor

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/application/Program.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/application/Program.java
@@ -1,12 +1,13 @@
 package M12.HerancaPolimorfismo.Heranca.application;
 
+import M12.HerancaPolimorfismo.Heranca.entities.Account;
 import M12.HerancaPolimorfismo.Heranca.entities.BusinessAccount;
 
 public class Program {
     public static void main(String[] args) {
 
-        BusinessAccount account = new BusinessAccount();
+        Account acc = new Account(1001, "Alex Green", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria Brown", 0.0, 500.0);
 
-        account.withdraw(); //Possibilita o acesso a todos os atrivutos da lasse
     }
 }
