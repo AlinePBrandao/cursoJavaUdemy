ecc4437147d48e551d7ed131ef05a334fd9f52c1
AlinePBrandao <alinep02brandao@gmail.com>
2025-08-13 16:45:39 -0300

    :construction: feat: Program - Downcasting e testes

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/application/Program.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/Heranca/application/Program.java
@@ -10,11 +10,15 @@ public class Program {
         Account acc = new Account(1001, "Alex Green", 0.0);
         BusinessAccount bacc = new BusinessAccount(1002, "Maria Brown", 0.0, 500.0);
 
-        //UPCASTING
+        //UPCASTING subclasse -> superclasse
 
         Account acc1 = bacc; //BusinessAccount também é Account, por isso pode ser atribuído. Ou seja, subclasse para superclasse
         Account acc2 = new BusinessAccount(1003, "Bob Blue", 0.0, 200.0);
         Account acc3 = new SavingsAccount(1004, "Anna Red", 0.0, 0.01);
 
+        //DOWNCASTING superclasse -> subclasse
+        BusinessAccount acc4 = (BusinessAccount)acc2; //necessário fazer casting, pois não se pode converter de Account para BusinessAccount
+        //acc2 é do tipo Account mesmo sendo instanciada como BusinessAccount
+        acc4.loan(100.0);
     }
 }
