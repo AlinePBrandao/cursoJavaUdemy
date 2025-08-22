ee974d4c7707c2e05cc527d9a6a8b7d8f947e32f
Aline Brandão <alinep02brandao@gmail.com>
2025-08-22 00:47:04 -0300

    Teste de novo

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/application/Program.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/application/Program.java
@@ -3,7 +3,8 @@ package M12.HerancaPolimorfismo.PaymentEmployees.application;
 public class Program {
     public static void main(String[] args) {
 
-        System.out.printf("Teste");
+        System.out.print("Teste");
+        System.out.print("Teste de novo");
     }
 
 }
