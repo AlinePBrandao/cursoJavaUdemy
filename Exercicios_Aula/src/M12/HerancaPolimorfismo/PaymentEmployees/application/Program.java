ce05e5cbcc666d6e3090557453fd71ebdfa45c86
Aline Brandão <alinep02brandao@gmail.com>
2025-09-17 15:50:31 -0300

    :construction: feat: Program - construção resposta padrão, list, for, char

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/application/Program.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/application/Program.java
@@ -1,9 +1,30 @@
 package M12.HerancaPolimorfismo.PaymentEmployees.application;
 
+import M12.HerancaPolimorfismo.PaymentEmployees.entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        List<Employee> list = new ArrayList<>();
 
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++){
+            System.out.println("Employee #" + i + " data");
+            System.out.print("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+
+        }
+
+        sc.close();
     }
 
 }
