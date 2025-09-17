5625af600451bad1d7f945f116249f5fde1e6034
Aline Brandão <alinep02brandao@gmail.com>
2025-09-17 15:41:28 -0300

    feat: OutSourceEmployee - atributos e métodos, extends e super,get e set,  override método payments

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/entities/OutSourceEmployee.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/entities/OutSourceEmployee.java
@@ -1,6 +1,24 @@
-package M12.HerancaPolimorfismo.PaymentEmployees.entities;public class OutSourceEmployee {
+package M12.HerancaPolimorfismo.PaymentEmployees.entities;
 
+public class OutSourceEmployee extends Employee {
 
+    private Double additionalCharge;
 
+    public OutSourceEmployee(String name, Integer hour, Double valuePerHour, Double additionalCharge) {
+        super(name, hour, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
 
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment(){
+        return super.payment() + additionalCharge * 1.1;
+    }
 }
