677506896f7eb499ce568ee5b9a7de7be1e0cd26
Aline Brandão <alinep02brandao@gmail.com>
2025-09-17 22:12:55 -0300

    :construction: feat: Program - hours, value per hour e if para remuneração adicional

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/application/Program.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/PaymentEmployees/application/Program.java
@@ -21,7 +21,18 @@ public class Program {
             System.out.println("Employee #" + i + " data");
             System.out.print("Outsourced (y/n)? ");
             char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine(); //consume quebra de linha pendente
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.print("Valua per hour: ");
+            double valuePerHour = sc.nextDouble();
 
+            if (ch == 'y'){
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+            }
         }
 
         sc.close();
