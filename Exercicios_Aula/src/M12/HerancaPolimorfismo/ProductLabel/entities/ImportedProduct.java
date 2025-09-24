f10c2d72a80fdeed9d23b51f56783c7573a2bdd9
Aline Brandão <alinep02brandao@gmail.com>
2025-09-24 15:51:20 -0300

    :construction: feat: ImportedProduct - herança de Product, variavel, constructor com super, métodos get e set, totalPrice e sobreposição priceTag

--- Exercicios_Aula/src/M12/HerancaPolimorfismo/ProductLabel/entities/ImportedProduct.java
+++ Exercicios_Aula/src/M12/HerancaPolimorfismo/ProductLabel/entities/ImportedProduct.java
@@ -1,4 +1,28 @@
 package M12.HerancaPolimorfismo.ProductLabel.entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+
+    private Double customsFee; // NOTE: taxa alfandegaria
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPrice() {
+        return getPrice() + customsFee;
+    }
+
+    @Override
+    public String priceTag(){
+        return getName() + "$ " + String.format("%.2f", totalPrice()) + "(Customs fee: $ " + String.format("%.2f", customsFee) + ")";
+    }
 }
