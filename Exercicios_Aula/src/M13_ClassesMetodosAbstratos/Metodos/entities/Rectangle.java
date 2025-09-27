b36a12e2adf099a8fdb350e820a07d87f147228d
Aline Brandão <alinep02brandao@gmail.com>
2025-09-27 03:09:06 -0300

    :construction: feat: Rectangle

--- Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Metodos/entities/Rectangle.java
+++ Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Metodos/entities/Rectangle.java
@@ -1,4 +1,36 @@
 package M13_ClassesMetodosAbstratos.Metodos.entities;
 
-public class Rectangle {
+import M13_ClassesMetodosAbstratos.Metodos.entities.enums.Color;
+
+public class Rectangle extends Shape {
+
+    private Double width;
+    private Double height;
+
+    public Rectangle(Color color, Double width, Double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    // TODO: sobrescrita do método
+    @Override
+    public double area() {
+        return 0;
+    }
 }
