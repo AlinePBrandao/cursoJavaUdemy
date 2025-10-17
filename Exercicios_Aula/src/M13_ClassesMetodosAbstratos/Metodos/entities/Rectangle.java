4d5c3e879f976569d438eedc0d9d9049932f597c
Aline Brandão <alinep02brandao@gmail.com>
2025-10-17 20:13:58 -0300

    :construction: feat: Rectangle - acabou viagem, acabou férias, acabou resfriado, acabou a palhaçada

--- Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Metodos/entities/Rectangle.java
+++ Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Metodos/entities/Rectangle.java
@@ -7,6 +7,7 @@ public class Rectangle extends Shape {
     private Double width;
     private Double height;
 
+
     public Rectangle(Color color, Double width, Double height) {
         super(color);
         this.width = width;
@@ -28,6 +29,10 @@ public class Rectangle extends Shape {
     public void setHeight(Double height) {
         this.height = height;
     }
+
+    // NOTE: classe precisa aplicar o metodo shape.area
+    // NOTE: Como a classe não é abstrata, ela é obrigada a sobrescrever o metodo abstrato da superclasse (area)
+
     // TODO: sobrescrita do método
     @Override
     public double area() {
