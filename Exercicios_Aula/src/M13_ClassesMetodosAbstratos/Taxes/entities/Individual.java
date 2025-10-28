adb60eae360370aff12d0d672ea3530b9d94ba6a
Aline Brandão <alinep02brandao@gmail.com>
2025-10-28 16:29:33 -0300

    :construction: feat: Individual

--- Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Taxes/entities/Individual.java
+++ Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Taxes/entities/Individual.java
@@ -1,6 +1,24 @@
 package M13_ClassesMetodosAbstratos.Taxes.entities;
 
-public class Individual {
+public class Individual extends TaxPayer {
 
+    private Double healthcareExpenses;
 
+    public Individual(String name, Double annualIncome, Double healthcareExpenses) {
+        super(name, annualIncome);
+        this.healthcareExpenses = healthcareExpenses;
+    }
+
+    public Double getHealthcareExpenses() {
+        return healthcareExpenses;
+    }
+
+    public void setHealthcareExpenses(Double healthcareExpenses) {
+        this.healthcareExpenses = healthcareExpenses;
+    }
+
+    @Override
+    public double tax() {
+        return getAnnualIncome() * tax();
+    }
 }
