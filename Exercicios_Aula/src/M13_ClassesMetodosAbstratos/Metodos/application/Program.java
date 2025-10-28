0c6fbea5cbefa109fcfcd07e9e91dd0e4fdb19f8
Aline Brandão <alinep02brandao@gmail.com>
2025-10-27 21:41:20 -0300

    :construction: feat: Program- construindo o program para ler os dados da forma. Color de enums.color

--- Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Metodos/application/Program.java
+++ Exercicios_Aula/src/M13_ClassesMetodosAbstratos/Metodos/application/Program.java
@@ -1,4 +1,27 @@
 package M13_ClassesMetodosAbstratos.Metodos.application;
 
+import M13_ClassesMetodosAbstratos.Metodos.entities.enums.Color;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of shapes: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=n; i++){
+            System.out.print("Shape #" + i + " data: ");
+            System.out.print("Rectangle or Circle (r/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+        }
+
+        sc.close();
+    }
 }
