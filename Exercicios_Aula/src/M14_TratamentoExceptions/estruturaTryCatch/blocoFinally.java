55623c20b0370be56b919e3ad8419c0de1815c83
Aline Brandão <alinep02brandao@gmail.com>
2025-11-03 23:21:36 -0300

    :construction: feat: blocoFinally: blocoFinally

---
+++ Exercicios_Aula/src/M14_TratamentoExceptions/estruturaTryCatch/blocoFinally.java
@@ -0,0 +1,31 @@
+package M14_TratamentoExceptions.estruturaTryCatch;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class blocoFinally {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\aline\\OneDrive\\Documentos\\Java Materiais\\in.txt.txt");
+        //NOTE: recebendo arquivo local
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file); //NOTE: Scanner instanciado apontando para o arquivo
+            while (sc.hasNextLine()){ //NOTE: se o arquivo existir, ler o arquivo
+                System.out.println(sc.nextLine());
+            }
+        }
+        //NOTE: caso o arquivo não seja encontrado a exceção será mostrada
+        catch (FileNotFoundException e){
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        //NOTE: independente de dar certo ou não o Scanner será fechado
+        finally {
+            if (sc != null){
+                sc.close();
+            }
+            System.out.println("Finally block executed");
+        }
+    }
+}
