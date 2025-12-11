7d31ff48d975aa345c483a663c7057c99dfa3cb2
Aline Brandão <alinep02brandao@gmail.com>
2025-12-11 19:16:40 -0300

    construction: build: Archives: Try with resources

--- Exercicios_Aula/src/M15_Archives/tryWithResources.java
+++ Exercicios_Aula/src/M15_Archives/tryWithResources.java
@@ -4,18 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class fileReader_BufferedReader {
+public class tryWithResources {
     public static void main(String[] args) {
-
         String path = "C:\\Users\\aline\\OneDrive\\Documentos\\Java Materiais\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
         //NOTE: tentar abrir e ler o arquivo
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        //NOTE: Streams declaradas direto no bloco try, FileReader será declarado no construtor do Buffered
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine(); //NOTE: le uma linha do arquivo
 
             while (line != null) { //NOTE: significa que leu a linha com sucesso
@@ -26,20 +21,6 @@ public class fileReader_BufferedReader {
         //NOTE: capturar uma exception
         catch (IOException e){
             System.out.println("Error: " + e.getMessage());
-        }
-        //NOTE: para tentar fechar as streams
-        finally {
-            try{
-                if (br != null){
-                    br.close();
-                }
-                if (fr != null){
-                    fr.close();
-                }
-            }
-            catch (IOException e){
-                e.printStackTrace(); //NOTE: mostra o erro
-            }
         }
     }
 }
